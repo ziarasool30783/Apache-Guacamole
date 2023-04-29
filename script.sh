5948655ee589d652775f37e242b56fdd1e7a53a4
Zia Rasool <zia.rasool@systemsltd.com>
2023-04-29 04:39:48 +0000

    Script FIle Commit

---
+++ script.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+#
+# check if docker is running
+if ! (docker ps >/dev/null 2>&1)
+then
+	echo "docker daemon not running, will exit here!"
+	exit
+fi
+echo "Preparing folder init and creating ./init/initdb.sql"
+mkdir ./init >/dev/null 2>&1
+mkdir -p ./nginx/ssl >/dev/null 2>&1
+chmod -R +x ./init
+docker run --rm guacamole/guacamole /opt/guacamole/bin/initdb.sh --postgres > ./init/initdb.sql
+echo "done"
+echo "Creating SSL certificates"
+openssl req -nodes -newkey rsa:2048 -new -x509 -keyout nginx/ssl/self-ssl.key -out nginx/ssl/self.cert -subj '/C=PK/ST=Punjab/L=Lahore/O=Systems/OU=IT/CN=www.systemsltd.com/emailAddress=zia.rasool@systemsltd.com'
+echo "You can use your own certificates by placing the private key in nginx/ssl/self-ssl.key and the cert in nginx/ssl/self.cert"
+echo "done"
+
+
+
+
+#Reference
+#https://www.ibm.com/docs/en/api-connect/2018.x?topic=overview-generating-self-signed-certificate-using-openssl
+#https://hub.docker.com/r/guacamole/guacamole
