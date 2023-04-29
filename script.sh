e07b4948397221b36451a0b8d36d023349cabf49
Zia Rasool <zia.rasool@systemsltd.com>
2023-04-29 04:41:50 +0000

    Script File Change

--- script.sh
+++ script.sh
@@ -14,7 +14,6 @@ docker run --rm guacamole/guacamole /opt/guacamole/bin/initdb.sh --postgres > ./
 echo "done"
 echo "Creating SSL certificates"
 openssl req -nodes -newkey rsa:2048 -new -x509 -keyout nginx/ssl/self-ssl.key -out nginx/ssl/self.cert -subj '/C=PK/ST=Punjab/L=Lahore/O=Systems/OU=IT/CN=www.systemsltd.com/emailAddress=zia.rasool@systemsltd.com'
-echo "You can use your own certificates by placing the private key in nginx/ssl/self-ssl.key and the cert in nginx/ssl/self.cert"
 echo "done"
 
 
